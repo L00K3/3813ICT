c08b3f689f50cdad305ea5ff6d6203edac161acb
L00K3 <lhensley97@gmail.com>
2018-07-20 16:22:36 +1000

    Add files via upload

--- Week2/index.js
+++ Week2/index.js
@@ -3,3 +3,18 @@ console.log("Hello World")
 
 //Task 2, 3
 //Created Git Repo
+
+//Task 4
+function add() {
+    let tally = 0
+    
+    try {
+        for (let arg in arguments) {
+            tally += arguments[arg]
+        }
+    } catch(error) { /* Non-Numeric Argument */ }
+    
+    return tally
+}
+
+console.log(add(5, 10))
